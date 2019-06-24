40593e6e413ef6079d3badffee12d0150f067805
Heber Sandoval <he3ers@gmail.com>
2019-06-24 18:48:49 -0400

    Created route for new courses and new action in CoursesController.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'courses#index'
 
+	# courses
+	get 'courses/new', to: 'courses#new'
+
 	# pages
 	get 'about', to: 'pages#about'
-	
+
 end
