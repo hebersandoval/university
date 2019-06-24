797fa39f65a748db0ba185bb54995ff5a39a8853
Heber Sandoval <he3ers@gmail.com>
2019-06-24 18:41:26 -0400

    PagesController added with an about action along with its corresponding view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'courses#index'
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root 'courses#index'
+
+	# pages
+	get 'about', to: 'pages#about'
+	
 end
