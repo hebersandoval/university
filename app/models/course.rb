d7e7876ea59379d44716b567df30640957924798
Heber Sandoval <he3ers@gmail.com>
2019-06-25 21:15:58 -0400

    Add simple validation for Course model, pass data to the view via the controller.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
+    validates :name, presence: true, length: { minimun: 5, maximum: 50 }
+    validates :short_name, presence: true, length: { minimun: 2, maximum: 5 }
+    validates :description, presence: true
 
 end
