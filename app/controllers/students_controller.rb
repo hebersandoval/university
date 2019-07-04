fd2b39c8036340bed0ae8e2827be452c25794cb9
Heber Sandoval <he3ers@gmail.com>
2019-07-04 12:24:34 -0400

    Refractor call to '.find()' in private method and use the before_action to call the show, edit and update methods.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,11 @@
 class StudentsController < ApplicationController
+    before_action :set_student, only: [:show, :edit, :update]
 
     def index
         @students = Student.all
     end
 
     def show
-        @student = Student.find(params[:id])
     end
 
     def new
@@ -23,11 +23,9 @@ class StudentsController < ApplicationController
     end
 
     def edit
-        @student = Student.find(params[:id])
     end
 
     def update
-        @student = Student.find(params[:id])
         if @student.update(student_params)
             flash[:success] = 'You have successfully updated you profile'
             redirect_to student_path(@student)
@@ -37,7 +35,12 @@ class StudentsController < ApplicationController
     end
 
     private
+
     def student_params
         params.require(:student).permit(:name, :email)
+    end
+
+    def set_student
+        @student = Student.find(params[:id])
     end
 end
