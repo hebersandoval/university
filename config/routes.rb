654acf0ab134e752f56519e7532808a01387b2f5
Heber Sandoval <he3ers@gmail.com>
2019-07-04 15:45:20 -0400

    Define routes for login/logout.

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
 	# students
 	resources :students, expect: [:destroy]
 
+	# login
+	get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	delete 'logout', to: 'sessions#destroy'
+
 end
