2c09700c3b285d9604a5aa1b273aa2c4f0f7c8c0
Heber Sandoval <he3ers@gmail.com>
2019-07-04 15:02:19 -0400

    Add has_secure_password to student model and added password_digest colum in students table.

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_06_26_012225) do
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
