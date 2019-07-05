38d990c3f1040343e7d8e85a0a9670df041fdbb7
Heber Sandoval <he3ers@gmail.com>
2019-07-05 17:54:06 -0400

    Refactor code in nav view and place login in helpers/application_helper.rb file.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    
+    def session_link
+        if (!logged_in?)
+            link_to('Login', login_path, class: "nav-link")
+        else
+            link_to('Log Out', logout_path, method: :delete, class: "nav-link")
+        end
+    end
 end
