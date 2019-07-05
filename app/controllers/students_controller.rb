8e090e458cc52de373f1fde372ae15d4bfeac932
Heber Sandoval <he3ers@gmail.com>
2019-07-05 19:31:24 -0400

    Deny access from controller lever to students edit and update action except if current_user is the same as student.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
     skip_before_action :require_user, only: [:new, :create]
     before_action :set_student, only: [:show, :edit, :update]
+    before_action :require_same_student, only: [:edit, :update]
 
     def index
         @students = Student.all
@@ -43,5 +44,12 @@ class StudentsController < ApplicationController
 
     def set_student
         @student = Student.find(params[:id])
+    end
+
+    def require_same_student
+        if current_user != @student
+            flash[:danger] = 'You can only edit your own profile'
+            redirect_to student_path(current_user)
+        end
     end
 end
