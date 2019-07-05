28ec5172df1ef06eed432cbeb43b03b9484a8abb
Heber Sandoval <he3ers@gmail.com>
2019-07-05 17:28:48 -0400

    Create current_user method to return if session[:student_id] is set and return the result in logged_in? method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+    
+    def current_user
+        # memoizing instance variable; perform query only if current_student doesn't exist
+        @current_student ||= Student.find(session[:student_id]) if session[:student_id]
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
