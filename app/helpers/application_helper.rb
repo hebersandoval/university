bee43d2ae7ad3091cc51b780498126100293fdb9
Heber Sandoval <he3ers@gmail.com>
2019-07-05 19:05:22 -0400

    Add a dropdown menu in the navbar for login/logout and refractor helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
     
     def session_link
         if (!logged_in?)
-            link_to('Login', login_path, class: "nav-link")
+            link_to('Login', login_path, class: "dropdown-item")
         else
-            link_to('Log Out', logout_path, method: :delete, class: "nav-link")
+            link_to('Log Out', logout_path, method: :delete, class: "dropdown-item")
         end
     end
 end
