08cc69c37a62f0c8cefecc67ef819038df3962b1
Heber Sandoval <he3ers@gmail.com>
2019-07-06 06:37:43 -0400

    Create StudentCourse model and added many to many table relationship between course and student models.

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,7 @@ class Course < ApplicationRecord
     validates :short_name, presence: true, length: { minimun: 2, maximum: 5 }
     validates :description, presence: true
 
+    has_many :student_courses
+    has_many :students, through: :student_courses
+
 end
